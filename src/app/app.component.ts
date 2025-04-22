0ca31402859efdecc4546f7a92905bcf72f571c3
Gustavo Teixeira <gustavo.teixeira2199@gmail.com>
2025-04-22 01:50:59 -0300

    CSS Style Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  buttonTitle = 'Título do botão';
-  buttonDisabled = false;
+  widthButton1 = '110px';
+  widthButton2 = 130;
 
-  onButtonClick() {
-    this.buttonTitle = 'Título alterado!';
-    this.buttonDisabled = !this.buttonDisabled;
+  stylesObj = {
+    width: '160px',
+    backgroundColor: 'grey'
+  }
+
+  updateStyleObj() {
+    console.log("updateStyleObj");
+    this.stylesObj.backgroundColor = 'green';
+    this.stylesObj.width = '200px';
+  }
+
+  updateStyleObjCorrect() {
+    console.log("updateStyleObjCorrect");
+    this.stylesObj = {
+      width: '170px',
+      backgroundColor: 'lightblue'
+    }
+  }
+
+  updateWidth() {
+    console.log('updateWidth')
+    this.widthButton2 = 200;
   }
 }

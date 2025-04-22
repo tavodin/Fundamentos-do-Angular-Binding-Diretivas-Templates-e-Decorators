df0c1e6ea29da8bc9fb885b67fe8a7d8938d2b23
Gustavo Teixeira <gustavo.teixeira2199@gmail.com>
2025-04-22 02:13:29 -0300

    CSS Class Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,30 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  widthButton1 = '110px';
-  widthButton2 = 130;
-
-  stylesObj = {
-    width: '160px',
-    backgroundColor: 'grey'
-  }
-
-  updateStyleObj() {
-    console.log("updateStyleObj");
-    this.stylesObj.backgroundColor = 'green';
-    this.stylesObj.width = '200px';
-  }
-
-  updateStyleObjCorrect() {
-    console.log("updateStyleObjCorrect");
-    this.stylesObj = {
-      width: '170px',
-      backgroundColor: 'lightblue'
-    }
-  }
-
-  updateWidth() {
-    console.log('updateWidth')
-    this.widthButton2 = 200;
-  }
+  isRedButton = true;
+  isGreen = true;
 }
